0debe005cc21bd679fcea7365d0c9c0563a1d6d5
BaseHype <ngwabenilihle@gmail.com>
2023-11-09 15:54:30 +0200

    basic setup

--- NumberGuessingGame/Program.cs
+++ NumberGuessingGame/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿// declare local variables
+Random rand = new Random();
+
+int winningNumber = rand.Next(0, 100);
+
+bool hasWon = false;
+
+// allow user to guess until the get it right
+do
+{
+
+} while (hasWon == false);
