c2c2c3c68c537c565dfd82aae51566b9d1cfdba5
BaseHype <ngwabenilihle@gmail.com>
2023-11-09 16:45:42 +0200

    multiple play turns functionality

--- NumberGuessingGame/Program.cs
+++ NumberGuessingGame/Program.cs
@@ -1,55 +1,71 @@
 ﻿// declare local variables
+using System.Security.AccessControl;
+
 Random rand = new Random();
 int selectedLevel;
 int winningNumber;
 int guessNumber;
-bool hasWon = false;
-bool levelValid = false;
 
+bool retryTurn = false;
 
-// user selects level of difficulty
+// repeat until user exits
 do
 {
-    Console.Write("Select Your Level of Difficulty: ");
-    string readLevel = Console.ReadLine();
+    bool hasWon = false;
+    bool levelValid = false;
 
-    selectedLevel = int.Parse(readLevel);
-
-    if (selectedLevel > 0 && selectedLevel <= 10)
+    // user selects level of difficulty
+    do
     {
-        Console.WriteLine($"You have selected level {selectedLevel} difficulty.");
-        levelValid = true;
-    }
+        Console.Write("Select Your Level of Difficulty: ");
+        string readLevel = Console.ReadLine();
 
-    Console.WriteLine();
-} while (levelValid == false);
+        selectedLevel = int.Parse(readLevel);
 
-// determine guessing range and select winning number
-int maxRange = selectedLevel * 10;
-winningNumber = rand.Next(0, maxRange);
+        if (selectedLevel > 0 && selectedLevel <= 10)
+        {
+            Console.WriteLine($"You have selected level {selectedLevel} difficulty.");
+            levelValid = true;
+        }
+
+        Console.WriteLine();
+    } while (levelValid == false);
+
+    // determine guessing range and select winning number
+    int maxRange = selectedLevel * 10;
+    winningNumber = rand.Next(0, maxRange);
 
 
-// prompt user to guess until they are correct
-do
-{
-    Console.Write($"Guess a number between 0 and {maxRange}: ");
-    string readResult = Console.ReadLine();
-
-    guessNumber = int.Parse(readResult);
-
-    if (guessNumber > winningNumber)
-        Console.WriteLine("Too high! Guess lower...");
-    else if (guessNumber < winningNumber)
-        Console.WriteLine("Too Low! Guess higher...");
-    else
+    // prompt user to guess until they are correct
+    do
     {
-        Console.WriteLine("YOU WIN!!");
-        hasWon = true;
-    }
+        Console.Write($"Guess a number between 0 and {maxRange}: ");
+        string readResult = Console.ReadLine();
 
-    Console.WriteLine();
-} while (hasWon == false);
+        guessNumber = int.Parse(readResult);
 
+        if (guessNumber > winningNumber)
+            Console.WriteLine("Too high! Guess lower...");
+        else if (guessNumber < winningNumber)
+            Console.WriteLine("Too Low! Guess higher...");
+        else
+        {
+            Console.WriteLine("YOU WIN!!");
+            hasWon = true;
+        }
+
+        Console.WriteLine();
+    } while (hasWon == false);
+
+    // prompt user to see if they want to continue
+    Console.WriteLine("Well done! If you wish to continue type \"Y\" else \"N\"");
+    string confirmation = Console.ReadLine();
+
+    retryTurn = confirmation.ToUpper().Trim() == "Y";
+
+} while (retryTurn == true);
+
+// display end game message
 Console.WriteLine("Thank you for playing this game.");
 Console.WriteLine("Press any key to finish.");
 Console.ReadKey();
